0dcc475a9aabfba4a0bda19180819e6d7174dab1
XUJING592@pingan.com.cn <XUJING592@pingan.com.cn>
2017-06-02 19:56:24 +0800

    base

---
+++ src/main/java/com/jazz/leetcode/algorithms/Solution_109.java
@@ -0,0 +1,12 @@
+package com.jazz.leetcode.algorithms;
+
+import com.jazz.leetcode.algorithms.base.ListNode;
+import com.jazz.leetcode.algorithms.base.TreeNode;
+
+public class Solution_109 {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/jazz/leetcode/algorithms/base/ListNode.java
@@ -0,0 +1,11 @@
+package com.jazz.leetcode.algorithms.base;
+
+/**
+ * Created by XUJING592 on 2017/6/2.
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
